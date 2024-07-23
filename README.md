a9a7e8e4959b6c1587dba782f82964682af2600f
Shing Lyu <shing.lyu@gmail.com>
2024-07-23 14:35:52 +0000

    feat: cleaned up smartcore serve script

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Prerequsite
+# Rust Machine Learning on Amazon SageMaker
+This repository contains example containers to run machine learning using Rust machine learning frameworks on Amazon SageMaker. 
+
+## Prerequsite
 * (Optional) [Install Rust](https://www.rust-lang.org/tools/install)
     * The Rust code is built in the Rust container, so you don't need to install them locally. But if you want to debug the Rust code locally you can install them.
 * Use SageMaker Studio to build the docker images
@@ -6,5 +9,36 @@
   * Enable docker support for your SageMaker Domain by following this [doc](https://docs.aws.amazon.com/sagemaker/latest/dg/studio-updated-local.html#studio-updated-local-enable). Do this BEFORE you create your code-editor application.
   * Open a code-editor application in SageMaker Studio
   * Install docker following the [doc](https://docs.aws.amazon.com/sagemaker/latest/dg/studio-updated-local.html#studio-updated-local-enable)
-# Building a Rust ML framework container
-* `cd` to the framework container folder. For example, `smartcore/`.
+
+## Usage
+Each top-level folder contains an example container definition for a Rust machine learning framework.
+* `smartcore`: [SmartCore](smartcore/src/bin/train-benchmark.rs) ([GitHub](smartcore/src/bin/train-benchmark.rs))
+* `linfa`: coming soon
+* `burn`: coming soon
+
+To use the container:
+1. Navigate into the folder containing the framewrok
+2. Modify the `src/bin/train.rs` to add your own model training logic
+3. Modify the `src/bin/serve.rs` to add your own model inference logic
+4. Build the container with `make build`
+
+### Local testing
+* `make test-train`: Test model training locally
+* `make test-serve`: start the model inference server for local testing
+  * `make test-predict`: Send a inference request to the inference server
+
+## Citation
+* Dataset
+  * Iris dataset: Fisher,R. A.. (1988). Iris. UCI Machine Learning Repository. https://doi.org/10.24432/C56C76
+
+## Roadmap
+- [ ] Add Linfa framework
+- [ ] Add Burn framework
+- [ ] Add Polars framework
+- [ ] Add more Rust ML framework
+- [ ] Support full BYOC input/output (e.g. `hyperparameters.json`, `resourceConfig.json`, `ouptut/failure`)
+- [ ] Support pipe mode
+- [ ] Support batch inference
+- [ ] Support data processing job
+- [ ] Add example Python notebook
+- [ ] Add example Rust notebook 
--- smartcore/src/bin/serve.rs
+++ smartcore/src/bin/serve.rs
@@ -1,12 +1,9 @@
-use actix_web::{get, post, guard, web, App, HttpServer, HttpResponse, Responder};
+use actix_web::{get, guard, web, App, HttpServer, HttpResponse, Responder};
 use ndarray::Array;
-use smartcore::dataset::*;
+
 // Random Forest
 use smartcore::tree::decision_tree_classifier::DecisionTreeClassifier;
-// Model performance
-use smartcore::metrics::mean_squared_error;
-use smartcore::model_selection::train_test_split;
-use std::error::Error;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::str;
@@ -16,8 +13,6 @@ use simple_logger::SimpleLogger;
 
 
 const MODEL_PATH: &str ="/opt/ml/model/model.bincode";
-//const MODEL_PATH: &str ="/tmp/model.bincode";
-
 
 #[derive(Serialize)]
 struct Response {
@@ -25,6 +20,7 @@ struct Response {
 }
 
 struct AppData {
+    // Change the model here as needed
     model: DecisionTreeClassifier<f32>
 }
 
@@ -33,7 +29,11 @@ async fn ping() -> impl Responder {
     HttpResponse::Ok()
 }
 
+// Change this invocations function to 
+// 1. Parse the inference payload
+// 2. Send it to the data.model for prediction
 async fn invocations(body: web::Bytes, data: web::Data<AppData>) -> impl Responder {
+    // Parsing the inference payload\
     let csv = str::from_utf8(&body).unwrap();
     let v: Vec<f32> = csv
         .split(',')
@@ -41,17 +41,24 @@ async fn invocations(body: web::Bytes, data: web::Data<AppData>) -> impl Respond
         .collect();
     info!("Received: {:?}", v);
     let x = Array::from_shape_vec( (1, v.len() ), v).unwrap();
+    
+    // Prediction
     let y_hat = data.model.predict(&x).unwrap();
     info!("Prediction: {:?}", y_hat);
+
+    // Send the response
     HttpResponse::Ok().json(Response{
         prediction: y_hat[0] as f32,
     })
 }
 
+// Change this main function to load the model
+// The model is loaded here instaed of in the invocation function so it can be reused across invocations 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     SimpleLogger::new().init().unwrap();
     info!("Loading model from {}", MODEL_PATH);
+    // Change the model as needed
     let model: DecisionTreeClassifier<f32> = {
         let mut buf: Vec<u8> = Vec::new();
         File::open(MODEL_PATH)
@@ -73,7 +80,7 @@ async fn main() -> std::io::Result<()> {
             .route(
                 "/invocations",
                 web::post()
-                    .guard(guard::Header("content-type", "text/csv"))
+                    .guard(guard::Header("content-type", "text/csv")) // Change the content type as needed
                     .to(invocations),
             )
     })
