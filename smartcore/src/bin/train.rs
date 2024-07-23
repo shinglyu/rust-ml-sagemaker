940fcd15d286139546a5841097dcac36b90819fd
Shing Lyu <shing.lyu@gmail.com>
2024-07-23 13:57:51 +0000

    fix: cleanup training script

--- smartcore/src/bin/train.rs
+++ smartcore/src/bin/train.rs
@@ -13,27 +13,28 @@ use csv;
 
 
 const TRAIN_DATA_PATH: &str = "opt/ml/input/data/training/iris.csv";
-//const TRAIN_DATA_PATH: &str = "./dataset/iris.csv";
 const MODEL_OUTPUT_PATH: &str ="/opt/ml/model/model.bincode";
-//const MODEL_OUTPUT_PATH: &str ="/tmp/model.bincode"; // For local testing
-// TODO: failure ouptut path
 
+// How to use this example script:
+// 1. Replace main() function with your own training logic
+// 2. Read the training dataset from TRAIN_DATA_PATH
+// 3. Write your model artifact to MODEL_OUTPUT_PATH
 fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new().init().unwrap();
+    
     info!("Loading dataset");
-    // Load dataset
-    //let iris_data = iris::load_dataset();
-    // Read the CSV
     let mut reader = csv::ReaderBuilder::new()
        .has_headers(false)
        .from_path(TRAIN_DATA_PATH)
        .expect(&format!("Fail to read the CSV at {}", TRAIN_DATA_PATH));
+
+       
     let data: Array2<String> = reader.deserialize_array2_dynamic()?;
     
+    // Parsing the training dataset into features (x) and target (y)
     let x_strings = data.slice(s![..,0..4]);
     let mut vec_x: Vec<f32> = Vec::new();
     for i in x_strings.iter() {
-        println!("{}", i);
         vec_x.push(i.parse().unwrap());
     }
     let x = Array::from_shape_vec( (data.nrows(), 4), vec_x )?;
@@ -49,43 +50,21 @@ fn main() -> Result<(), Box<dyn Error>> {
         vec_y.push(t_f);
     }
     let y = Array::from_shape_vec(data.nrows(), vec_y).unwrap();
-    // Transform dataset into a NxM matrix
-    /*
-    let x = Array::from_shape_vec(
-        (iris_data.num_samples, iris_data.num_features),
-        iris_data.data,
-    )?;
-    */
-    // These are our target class labels
-    /*
-    let y = Array::from_shape_vec(iris_data.num_samples, iris_data.target)?;
-    */
-    // Split dataset into training/test (80%/20%)
-    //let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
-    //println!("{}", x_test);
+    
     info!("Dataset loaded and preprocessed");
     info!("Training model");
-    // Random Forest
+    // Train a Random Forest
     let rf_model = DecisionTreeClassifier::fit(&x, &y, Default::default())?;
-        // .and_then(|rf| rf.predict(&x_test)).unwrap();
-    // Calculate test error
-    /*
-    println!("MSE: {}", mean_squared_error(&y_test, &y_hat_rf));
-    println!("{:?}", &y_test);
-    println!("{:?}", &y_hat_rf);
-    println!("Diff: {}", &y_hat_rf- &y_test);
-    */
     
     // Save the model
-    info!("Model training succeede");
+    info!("Model training succeeded");
     {
-        info!("Saving model to {}", MODEL_OUTPUT_PATH);
+        info!("Saving model artifact to {}", MODEL_OUTPUT_PATH);
         let model_bytes = bincode::serialize(&rf_model).expect("Can not serialize the model");
         File::create(MODEL_OUTPUT_PATH)
             .and_then(|mut f| f.write_all(&model_bytes))
             .expect("Can not persist model");
         info!("Model saved");
     }
-    
     Ok(())
 }
