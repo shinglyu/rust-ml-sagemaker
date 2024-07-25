b36b752969920befff63a5cd337badf84bfa3d2d
Shing Lyu <shing.lyu@gmail.com>
2024-07-25 13:48:51 +0000

    feat: linfa training cleaned up

--- linfa/src/bin/train.rs
+++ linfa/src/bin/train.rs
@@ -42,48 +42,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect();
     let x = Array::from_shape_vec((data.nrows(), 4), parsed_x_strings)?;
     
-    /*
     let y_array_view = data.slice(s![.., 4]);
-    let y: Array<String, _> = y_array_view.iter().map(|s| s.to_string()).collect();
-    */
-    let y_strings = data.slice(s![.., 4]);
-    let mut vec_y: Vec<&str> = Vec::new();
-    for t in y_strings.iter() {
-        vec_y.push(t);
-    }
-    let y = Array::from_shape_vec(data.nrows(), vec_y).unwrap();
-
-
+    let y: Array<&str, _> = y_array_view.iter().map(|s| s.as_str()).collect();
     
-    info!("Dataset loaded and preprocessed");
     let data = Dataset::new(x, y); // TODO: with feature names // TODO use map_targets
-    // Parsing the training dataset into features (x) and target (y)
-    /*
-    let x_strings = data.slice(s![..,0..4]);
-    let mut vec_x: Vec<f64> = Vec::new();
-    for i in x_strings.iter() {
-        vec_x.push(i.parse().unwrap());
-    }
-    let x = Array::from_shape_vec( (data.nrows(), 4), vec_x )?;
-    let y_strings = data.slice(s![.., 4]);
-    let mut vec_y: Vec<f64> = Vec::new();
-    for t in y_strings.iter() {
-        let t_f = match t.as_str() {
-            "Iris-setosa" => 0.,
-            "Iris-versicolor" => 1.,
-            "Iris-virginica" => 2.,
-            _ => 0.,
-        };
-        vec_y.push(t_f);
-    }
-    let y = Array::from_shape_vec(data.nrows(), vec_y).unwrap();
-    */
     
-    info!("Dataset loaded and preprocessed");
     let mut rng = SmallRng::seed_from_u64(42);
     let (train, test) = data.shuffle(&mut rng).split_with_ratio(0.8);
+    info!("Dataset loaded and preprocessed");
 
-    println!("Training model with Gini criterion ...");
+    info!("Training model with Gini criterion ...");
     let gini_model = DecisionTree::params()
         .split_quality(SplitQuality::Gini)
         .max_depth(Some(100))
@@ -91,39 +59,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         .min_weight_leaf(1.0)
         .fit(&train)?;
 
+    // Optional: showing that the model works
     let gini_pred_y = gini_model.predict(&test);
     let cm = gini_pred_y.confusion_matrix(&test)?;
 
-    println!("{:?}", cm);
-    println!(
+    info!("{:?}", cm);
+    info!(
         "Test accuracy with Gini criterion: {:.2}%",
         100.0 * cm.accuracy()
     );
 
     let feats = gini_model.features();
-    println!("Features trained in this tree {:?}", feats);
+    info!("Features trained in this tree {:?}", feats);
 
-    /*
-    println!("Training model with entropy criterion ...");
-    let entropy_model = DecisionTree::params()
-        .split_quality(SplitQuality::Entropy)
-        .max_depth(Some(100))
-        .min_weight_split(10.0)
-        .min_weight_leaf(10.0)
-        .fit(&train)?;
-
-    let entropy_pred_y = entropy_model.predict(&test);
-    let cm = entropy_pred_y.confusion_matrix(&test)?;
-
-    println!("{:?}", cm);
-    println!(
-        "Test accuracy with Entropy criterion: {:.2}%",
-        100.0 * cm.accuracy()
-    );
-
-    let feats = entropy_model.features();
-    println!("Features trained in this tree {:?}", feats);
-    */
 
     // Export model
     {
@@ -134,7 +82,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             .expect("Can not persist model");
         info!("Model saved");
     }
-    // Exporting the tree diagram
+
+    // Optional:  Exporting the tree diagram
     let mut tikz = File::create(format!("{}/decision_tree_example.tex", MODEL_OUTPUT_PATH)).unwrap();
     tikz.write_all(
         gini_model
@@ -144,9 +93,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .as_bytes(),
     )
     .unwrap();
-    println!(" => generate Gini tree description with `latex decision_tree_example.tex`!");
-
-
+    info!(" => generate Gini tree description with `latex decision_tree_example.tex`!");
 
     Ok(())
 }
